a1a47712e82b0864564b0dcb67cd53685bf51cf8
Francis Waihiga <francisgwaihiga@gmail.com>
2023-10-12 03:55:15 +0000

    corrections on the structure and comments of the base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,94 +1,87 @@
 #!/usr/bin/python3
-""" Base model class module
+"""
+This module has Basemodel class that defines attributes and methods of
+subsequent classes
 """
 
+
+import datetime
 from uuid import uuid4
-from datetime import datetime
-
 from models import storage
-
-CURRENT_DATE = datetime.now()
 
 
 class BaseModel:
-    """ BaseModel that defines all common,
-        attributes/methods for other classes.
+    """
+    The Basemodel class that has attributes to be inherited by
+    child classes.
     """
 
-    def __init__(self, *args, **kwargs) -> None:
-        """ method to initalize class BaseModel
-            Args:
-                args: a list of positional arguments.
-                kwargs: a dict of keyword arguments.
+    def __init__(self, *args, **kwargs):
         """
-        if len(kwargs) > 0:  # if kwargs is not empty
-            attrs = self.__dict__
+        Used to initalize class BaseModel
+        Args:
+            args: list of arguments.
+            kwargs: a dictionary of keyworded arguments.
+        """
+        if len(kwargs) > 0:
+            attributes = self.__dict__
 
-            for key, val in kwargs.items():
+            for key, v in kwargs.items():
                 if key == "__class__":
                     continue
-                elif key == "updated_at" or key == "created_at":
-                    attrs[key] = self.__to_datetime(val)
+                if key == "created_at" or key == "updated_at":
+                    attributes[key] = self.__to_datetime(v)
                 else:
-                    attrs[key] = val
+                    attributes[key] = v
         else:
             self.id = str(uuid4())
-            self.created_at = CURRENT_DATE
-            self.updated_at = CURRENT_DATE
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
             storage.new(self)
 
-    def __str__(self) -> str:
-        """ get string respresentation of class BaseModel.
-            Return:
-                  string represtation of BaseModel.
+    def __str__(self):
         """
-        class_name = self.__class__.__name__
-        return "[{0}] ({1}) {2}".format(class_name, self.id, self.__dict__)
+        Returns a string respresentation of the class BaseModel.
+        """
+        i = "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return (i)
 
-    def save(self) -> None:
-        """ update public instance updated_at
+    def save(self):
+        """
+        Updates public instance updated_at with current datetime
         """
 
-        self.updated_at = CURRENT_DATE  # update updated_at attribute
+        self.updated_at = datetime.datetime.now()
         storage.save()
 
-    def to_dict(self) -> dict:
-        """ get and return all keys/values of __dict__
-           Return:
-                 a dictionary containing all,
-                 keys/values of __dict__ of the instance.
+    def to_dict(self):
         """
-        base_dict = self.__dict__
-        base_dict = dict(self.__dict__)
-        base_dict["created_at"] = self.__to_string(base_dict["created_at"])
-        base_dict["updated_at"] = self.__to_string(base_dict["updated_at"])
-        base_dict["__class__"] = self.__class__.__name__
-
-        return base_dict
-
-    def __to_string(self, date: datetime) -> str:
-        """ convert datetime object to string in ISO format
-            Return:
-                  string represetation of datetime object in ISO format.
+        Returns all keys/values of __dict__(dictionary)
         """
-        return date.isoformat()
+        dic = self.__dict__
+        dic = dict(self.__dict__)
+        dic["created_at"] = self.convert_to_string(dic["created_at"])
+        dic["updated_at"] = self.convert_to_string(dic["updated_at"])
+        dic["__class__"] = self.__class__.__name__
 
-    def __to_string(self, date_obj: datetime) -> str:
-        """ convert datetime object to string in ISO format.
+        return (dic)
+
+    def convert_to_string(self, date_obj: datetime):
+        """
+        Converts and returns datetime obj to string in ISO format.
             Args:
-                date_obj: datetime object.
-            Return:
-                  string represetation of datetime object in ISO format.
+                date_obj: The datetime object.
         """
 
-        return date_obj.isoformat()
+        return (date_obj.isoformat())
 
-    def __to_datetime(self, date_string: str) -> datetime:
-        """ convert string represantation of date to datetime object.
+    def convert_to_datetime(self, date_str: str):
+        """
+        Converts date string to datetime object.
             Args:
-                date_string: datetime string.
+                date_string: the date string.
             Return:
-                  datetime object from string.
+                  It returns the datetime object from string.
         """
 
-        return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f")
+        return (datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f"))
